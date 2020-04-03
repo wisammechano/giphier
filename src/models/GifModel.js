5b4e04746665237d68c34bdfbd47fcd092d017fa
Wisam Naji <guitar90rocker@gmail.com>
2020-04-03 03:37:37 +0300

    added sizes

--- src/models/GifModel.js
+++ src/models/GifModel.js
@@ -1,6 +1,38 @@
 import BaseModel from "./BaseModel";
 
-const size = "downsized";
+const sizes = {
+  downsized: {
+    large: "downsized_large",
+    medium: "downsized_medium",
+    base: "downsized",
+    small: "downsized_small"
+  },
+
+  stills: {
+    fhs: "fixed_height_small_still",
+    ds: "downsized_still",
+    fh: "fixed_height_still",
+    fws: "fixed_width_small_still",
+    o: "original_still",
+    fw: "fixed_width_still",
+    w480: "480w_still"
+  },
+  fixed_width: {
+    downsampled: "fixed_width_downsampled",
+    small: "fixed_width_small",
+    base: "fixed_width"
+  },
+  fixed_height: {
+    downsampled: "fixed_height_downsampled",
+    small: "fixed_height_small",
+    base: "fixed_height"
+  },
+  preview: { webp: "preview_webp", base: "preview", gif: "preview_gif" },
+  original: { base: "original", mp4: "original_mp4" },
+  looping: "looping"
+};
+
+const size = sizes.preview.gif;
 
 class GifModel extends BaseModel {
   constructor(json) {
